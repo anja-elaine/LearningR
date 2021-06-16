2525d4af219056b90fddf32e308ca39a3040014e
Anja Elaine Sørensen <elaine@ruc.dk>
2021-06-16 10:47:25 +0200

    Playing with data wrangling

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -13,3 +13,37 @@ summary(NHANES)
 
 # Look over the dataset documentation
 ?NHANES
+
+# Select one column by its name, without quotes
+select(NHANES, Age)
+# Select two or more columns by name, without quotes
+select(NHANES, Age, Weight, BMI)
+# To *exclude* a column, use minus (-)
+select(NHANES, -HeadCirc)
+# All columns starting with letters "BP" (blood pressure)
+select(NHANES, starts_with("BP"))
+# All columns ending in letters "Day"
+select(NHANES, ends_with("Day"))
+# All columns containing letters "Age"
+select(NHANES, contains("Age"))
+
+# Save the selected columns as a new data frame
+# Recall the style guide for naming objects
+nhanes_small <- select(NHANES, Age, Gender, Height,
+                       Weight, BMI, Diabetes, DiabetesAge,
+                       PhysActiveDays, PhysActive, TotChol,
+                       BPSysAve, BPDiaAve, SmokeNow, Poverty)
+
+# View the new data frame
+nhanes_small
+
+# Rename all columns to snake case
+nhanes_small <- rename_with(nhanes_small, snakecase::to_snake_case)
+
+# Have a look at the data frame
+nhanes_small
+
+rename(nhanes_small, sex = gender)
+nhanes_small <- rename(nhanes_small, sex = gender)
+# View data frame
+nhanes_small
